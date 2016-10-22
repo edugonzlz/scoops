4e6e2c66a467533573fbe726b96884d1dd9b760f
Eduardo González <fonologico@gmail.com>
2016-10-22 12:05:55 +0200

    Commit inicial. Importado framework AzureMobile y creado Bridging-header

---
+++ scoops/scoops-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  scoops-Bridging-Header.h
+//  scoops
+//
+//  Created by Edu González on 22/10/16.
+//  Copyright © 2016 Edu González. All rights reserved.
+//
+
+#ifndef scoops_Bridging_Header_h
+#define scoops_Bridging_Header_h
+
+#import <MicrosoftAzureMobile/MicrosoftAzureMobile.h>
+
+#endif /* scoops_Bridging_Header_h */
